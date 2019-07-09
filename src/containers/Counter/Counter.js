9f743d7b4740e3783fabe9b37f13444eaf15351b
Jack Gamboa <jgamboa929@gmail.com>
2019-07-09 16:41:52 -0500

    Counter.js - add dispatch actions.

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import CounterControl from "../../components/CounterControl/CounterControl";
 import CounterOutput from "../../components/CounterOutput/CounterOutput";
@@ -40,26 +41,65 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <CounterOutput value={this.state.counter} />
+        <CounterOutput value={this.props.ctr} />
         <CounterControl
           label="Increment"
-          clicked={() => this.counterChangedHandler("inc")}
+          clicked={this.props.onIncrementCounter}
         />
         <CounterControl
           label="Decrement"
-          clicked={() => this.counterChangedHandler("dec")}
+          clicked={this.props.onDecrementCounter}
         />
-        <CounterControl
-          label="Add 5"
-          clicked={() => this.counterChangedHandler("add", 5)}
-        />
+        <CounterControl label="Add 5" clicked={this.props.onAddCounter} />
         <CounterControl
           label="Subtract 5"
-          clicked={() => this.counterChangedHandler("sub", 5)}
+          clicked={this.props.onSubtractCounter}
         />
       </div>
     );
   }
 }
 
-export default Counter;
+/*
+  We pass two pieces of info to connect ()
+  1 slice of data we want to 'get' (Which part of application state is interesting to us?)
+  2 action type we want to 'set'
+
+  The function set to mapStateToProps will eventually be executed by the 'react-redux' package because we pass state to it
+  - Below (state) is the state from redux which will be retreived and passed down to this Counter component as props.
+  - mapStateToProps() returns an object that is a map of slices of state from redux and assigned to user defined properties which is passed on
+  to Counter component.
+
+*/
+
+/* 
+  So here we are saying please give me the state.counter we have in our global app state managed by redux
+  and give to me in the form of a property 'ctr' which we can use in this component called Counter using props.counter.
+  We will no longer manage state using the local class component's 'state'.
+
+  This is the 'get' part of subscribe
+*/
+const mapStateToProps = state => {
+  return {
+    ctr: state.counter
+  };
+};
+
+/*
+  Here we say which kind of actions do want to dispatch to the store.
+  Will need action type and payload.
+  This receives the dispatch() function which we can execute as an argumen
+*/
+const mapDispatchToProps = dispatch => {
+  return {
+    onIncrementCounter: () => dispatch({ type: "INCREMENT" }),
+    onDecrementCounter: () => dispatch({ type: "DECREMENT" }),
+    onAddCounter: () => dispatch({ type: "ADD", val: 5 }),
+    onSubtractCounter: () => dispatch({ type: "SUBTRACT", val: 5 })
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,34 @@
 const initialState = { counter: 0 };
 
 const reducer = (state = initialState, action) => {
+  if (action.type === "INCREMENT") {
+    return {
+      ...state, // Here didnt have to copy state object b/c counter is the only property.  Kept here anyways.
+      counter: state.counter + 1
+    };
+  }
+
+  if (action.type === "DECREMENT") {
+    return {
+      ...state, // Here didnt have to copy state object b/c counter is the only property.  Kept here anyways.
+      counter: state.counter - 1
+    };
+  }
+
+  if (action.type === "ADD") {
+    return {
+      ...state,
+      counter: state.counter + action.val
+    };
+  }
+
+  if (action.type === "SUBTRACT") {
+    return {
+      ...state,
+      counter: state.counter - action.val
+    };
+  }
+
   return state;
 };
 
