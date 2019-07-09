40a09e37fc4b90ca24a587e9ff699c5dcd663948
Jack Gamboa <jgamboa929@gmail.com>
2019-07-09 13:07:32 -0500

    index.js - set up redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+import reducer from "./store/reducer";
+
+const reduxDevToolExtension =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(reducer, reduxDevToolExtension);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
